0bfa2a1096b1c64fd19fa0c605eebbb6cfe7b202
unknown <adiprasetyaxyz@icloud.com>
2023-07-06 06:58:43 -0700

    Learn Data Structure Sets

---
+++ sets.py
@@ -0,0 +1,16 @@
+
+#Create empity set
+s = set()
+
+#add elements to set
+s.add(1)
+s.add(2)
+s.add(3)
+s.add(4)
+s.add(3)
+
+#Remove Elements
+s.remove(2)
+
+print(s)
+print(f"s has {len(s)} elements")
