aeb584bd26682fcb9260c0d0b3af715343661e38
unknown <adiprasetyaxyz@icloud.com>
2023-07-06 06:33:40 -0700

    make simple condition is else

---
+++ condition.py
@@ -0,0 +1,8 @@
+n = int(input("input number :"))
+#add variable (int) for change n type data, for default data type is str
+if n > 0:
+    print("n is positive")
+elif n < 0:
+    print("n is negative")
+else:
+    print("n is zero")
