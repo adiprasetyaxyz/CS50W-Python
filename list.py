5d502e2a509586f58a9f6940376ff2bf7b556af7
unknown <adiprasetyaxyz@icloud.com>
2023-07-06 06:58:09 -0700

    Learn Data Struktur llist

---
+++ list.py
@@ -0,0 +1,10 @@
+# define list of names
+names = ["Harry", "Ron", "Hermoine", "Ginny"]
+
+print(names[2])
+#add new list names
+names.append("Draco")
+#function to sort list in alphabet
+names.sort()
+
+print(names)
